ce1aa86f930c935187f88deb96f89c0258e0ac80
Samuel Clay <samuel@ofbrooklyn.com>
2019-10-13 19:31:18 -0400

    Testing distance.

--- static/js/snapshot.js
+++ static/js/snapshot.js
@@ -53,23 +53,33 @@ window.collectGps = function() {
 };
 window.processGpsPosition = function(position) {
   CM.geolocation = position;
+  CM.seenLocations = CM.seenLocations || [];
+  CM.lastPos = CM.lastPos || {lat: 0, long: 0};
+  
   var pos = {
     lat: position.coords.latitude,
     lng: position.coords.longitude
   };
 
-  if (CM.googleMap) {  
+  if (CM.googleMap) {
     CM.googleMap.setCenter(pos);
     CM.googleMap.setZoom(16);
+    const distanceFromLastPoint = window.distanceBetweenLatLongs(pos.lat, CM.lastPos.lat, pos.lng, CM.lastPos.lng);
+    CM.lastPos = pos;
+    
+    if (distanceBetweenLatLongs < 20) {
+      return;
+    }
     
     const rotation = position.coords.heading - 90;
-    var chevron = {
+    const chevron = {
       path: 'M 13 3 L 7 12 L 13 21 L 17 21 L 11 12 L 17 3 L 13 3 z',
       strokeColor: '#F00',
       fillColor: '#F00',
       fillOpacity: 1,
       rotation: rotation
     };
+    const chevronOld = $.extend({}, chevron, {fillColor: '#FF0', strokeColor: '#FF0'});
     new google.maps.Marker({
       position: CM.googleMap.getCenter(),
       icon: chevron,
@@ -77,6 +87,22 @@ window.processGpsPosition = function(position) {
     });
   }
 };
+window.distanceBetweenLatLongs = function(lat1, lon1, lat2, lon2) {
+  var R = 6371e3; // metres
+  var φ1 = lat1.toRadians();
+  var φ2 = lat2.toRadians();
+  var Δφ = (lat2-lat1).toRadians();
+  var Δλ = (lon2-lon1).toRadians();
+
+  var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+          Math.cos(φ1) * Math.cos(φ2) *
+          Math.sin(Δλ/2) * Math.sin(Δλ/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+  var d = R * c;
+  
+  return d;
+}
 window.collectGpsInterval = setInterval(window.collectGps, 5 * 1000);
 navigator.geolocation.watchPosition(window.processGpsPosition, function() {
   console.log(['No permission for GPS', true]);
