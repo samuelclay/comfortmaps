071162b80c5a8dc304fe7b5fd5d64e55a8633c2b
Samuel Clay <samuel@ofbrooklyn.com>
2019-12-23 18:13:06 -0500

    Updating firmware

---
+++ firmware/.dep/spi.o.d
@@ -0,0 +1,32 @@
+spi.o: spi.c /usr/local/CrossPack-AVR-20131216/avr/include/avr/io.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/sfr_defs.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/inttypes.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/stdint.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/iotn2313a.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/portpins.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/common.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/version.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/fuse.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/lock.h spi.h
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/io.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/sfr_defs.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/inttypes.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/stdint.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/iotn2313a.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/portpins.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/common.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/version.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/fuse.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/lock.h:
+
+spi.h:
---
+++ firmware/.dep/wake-on-shake.o.d
@@ -0,0 +1,54 @@
+wake-on-shake.o: wake-on-shake.c \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/io.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/sfr_defs.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/inttypes.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/stdint.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/iotn2313a.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/portpins.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/common.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/version.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/fuse.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/lock.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/sleep.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/interrupt.h serial.h \
+ eeprom.h wake-on-shake.h interrupts.h spi.h ADXL362.h xl362.h ui.h
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/io.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/sfr_defs.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/inttypes.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/stdint.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/iotn2313a.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/portpins.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/common.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/version.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/fuse.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/lock.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/sleep.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/interrupt.h:
+
+serial.h:
+
+eeprom.h:
+
+wake-on-shake.h:
+
+interrupts.h:
+
+spi.h:
+
+ADXL362.h:
+
+xl362.h:
+
+ui.h:
