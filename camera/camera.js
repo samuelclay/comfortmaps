5b73a2cbafb3d894b055e9abf4d3460fff6d8b5a
Samuel Clay <samuel@ofbrooklyn.com>
2020-02-07 18:20:17 -0500

    Watching buttons.

--- camera/camera.js
+++ camera/camera.js
@@ -1,27 +1,36 @@
-var gpio = require('rpi-gpio');
+const gpio = require('rpi-gpio');
+const Raspistill = require('node-raspistill').Raspistill;
+const camera = new Raspistill({
+    verticalFlip: true,
+    quality: 10,
+});
 
-
-class ButtonWatch {
-    const buttons = [18, 23, 22, 17, 27];
-    var pressed = new Set();
-    
+class ButtonWatch {    
     constructor() {
-        gpio.on('change', this.buttonChange);
+        this.buttons = [18, 23, 22, 17, 27];
+        this.pressed = new Set();
+        
+        gpio.setMode(gpio.MODE_BCM);
+        gpio.on('change', this.buttonChange.bind(this));
 
         this.buttons.forEach((button) => {
-            console.log(['Setting up', button]);
             gpio.setup(button, gpio.DIR_IN, gpio.EDGE_BOTH);
         });        
+
+        console.log(' ---> Starting Comfort Maps', this.buttons);
     }
     
     buttonChange(channel, value) {
         const channelPressed = this.pressed.has(channel);
-        console.log(['Channel', channel, value]);
+        // console.log(['Channel', channel, value]);
         
         if (value == 0) {
             if (!channelPressed) {
                 this.pressed.add(channel);
-                console.log("Pressed " + channel);
+                console.log(" ---> Pressed " + channel);
+                camera.takePhoto().then((photo) => {
+                    console.log(['Photo', photo]);
+                });
             }
         } else if (value == 1) {
             if (channelPressed) {
