b0b78062aedc64f219697b6a11be262cfaa069cf
Samuel Clay <samuel@ofbrooklyn.com>
2020-05-04 21:37:03 -0400

    Wakeup lasts 90 seconds.

--- camera/wakeup/wakeup.c
+++ camera/wakeup/wakeup.c
@@ -1,4 +1,6 @@
 #include <wiringPi.h>
+#include <time.h>
+#include <unistd.h>
 
 int main(void)
 {
@@ -7,10 +9,24 @@ int main(void)
     const int wakeup = 7;
 
     wiringPiSetup();
-
     pinMode(wakeup, OUTPUT);
 
-    digitalWrite(wakeup, HIGH);
 
+    time_t start, end;
+    double elapsed;  // seconds
+    start = time(NULL);
+    int terminate = 1;
+    while (terminate) {
+      end = time(NULL);
+      elapsed = difftime(end, start);
+      if (elapsed >= 90.0 /* seconds */)
+        terminate = 0;
+      else  // No need to sleep when 90.0 seconds elapsed.
+        sleep(1);
+
+      digitalWrite(wakeup, HIGH);
+
+    }
+    
     return 0;
 }
